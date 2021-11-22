0421873a1bb8b6fb40e742844b1683ed32a8958f
nandweza <allannandweza@gmail.com>
2021-11-22 07:03:30 -0800

    added task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """ returns a tuple with the length of a string
+         and its first character."""
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
