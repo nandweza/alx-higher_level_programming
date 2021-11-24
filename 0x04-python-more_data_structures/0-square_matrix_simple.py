3e527fd90437ce45aeb0984e67a2479ba259232c
nandweza <allannandweza@gmail.com>
2021-11-24 09:15:33 -0800

    Innitial commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """computes the square value of all integers of a matrix."""
+    new_matrix = [[i**2 for i in row] for row in matrix]
+    return new_matrix
