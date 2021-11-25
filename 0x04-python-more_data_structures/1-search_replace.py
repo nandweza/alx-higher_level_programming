f5902bec8ba8d3fd63fc5423eef3a02c2d8d2a5f
nandweza <allannandweza@gmail.com>
2021-11-24 22:59:42 -0800

    added searc and replace to my code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """replaces all occurrences of an element by another in a new list."""
-    new_list = [89 if x == 2 else x for x in my_list]
+    new_list = []
+    for x in range(len(my_list)):
+        if my_list[x] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[x])
     return new_list
