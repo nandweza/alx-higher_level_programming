08859aa9b0c720462c429a9f43ab4fccab0494fe
nandweza <allannandweza@gmail.com>
2021-12-06 05:31:29 -0800

    Fixed Errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     """ function that prints x elements of a list."""
     count = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except IndexError:
-            break
-        else:
             count += 1
-        print()
-        return count
+        except:
+            pass
+    print()
+    return count
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     """function that prints an integer with "{:d}".format()."""
     try:
-        print("{:d}".format(i), end="")
+        print("{:d}".format(value))
+        return True
     except:
         return False
-    else:
-        return True
