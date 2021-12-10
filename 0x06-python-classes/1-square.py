72d3ae79a1b5412a2dd5445869ea0d294de03fa9
nandweza <allannandweza@gmail.com>
2021-12-09 22:37:50 -0800

    added task 1 to 6

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square with,
+     private instance attribute: size.
+    """
+    def __init__(self, size):
+        """Initializes the data"""
+        self.__size = size
