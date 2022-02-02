7e9c176d026d95c4d13246e06eb517177f4ab6b4
nandweza <allannandweza@gmail.com>
2022-02-02 07:39:25 -0800

    Initial commit

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2.
+-- creating database hbtn0d_2
+CREATE DATABASE hbtn_0d_2;
+-- creating user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+-- granting SELECT privileges for user_0d_2
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
