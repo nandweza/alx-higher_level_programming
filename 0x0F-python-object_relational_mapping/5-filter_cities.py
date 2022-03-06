841eb80e1856e65a25ab06b49e3bcfb495dca1b5
nandweza <allannandweza@gmail.com>
2022-03-06 07:28:36 -0800

    fixed code

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     cur.execute("SELECT cities.name\
                 FROM cities LEFT JOIN states\
                 ON states.id = cities.state_id\
-                WHERE states.name=%s\
+                WHERE states.name = %s\
                 ORDER BY cities.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
-    print(",".join([row[0] for row in rows]))
+    print(", ".join([row[0] for row in rows]))
     cur.close()
     db.close()
