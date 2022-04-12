c4bb11e58e80014b604f4d651af2d61daf43c9d5
nandweza <allannandweza@gmail.com>
2022-04-12 06:44:16 -0700

    fixed code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,8 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, result) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(result);
-  }
-});
+try {
+  fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, result) { if (err) console.log(err); });
+} catch (err) {
+  console.log(err);
+}
